7a1fc77f7b8d133fc072a50b2ed3918c9fdbaab5
clase 1 <albertoarauzsalazar65@gmail.com>
2023-03-23 16:58:00 -0600

    trabajohtml

---
+++ prac.py
@@ -0,0 +1,4 @@
+nombre_completo = input("Ingrese su nombre completo: ")
+print("Nombre completo en minúsculas:", nombre_completo.lower())
+print("Nombre completo en mayúsculas:", nombre_completo.upper())
+print("Nombre completo en mayúsculas:", nombre_completo.title())
